e06cc3de27eebdd3d3eba867435b84109c2897d6
Ely Santos <ely.franklin@acad.ifma.edu.br>
2021-06-20 18:20:31 -0300

    Uso de códigos do repositorio bolsonaro-replace

--- background.js
+++ background.js
@@ -1,36 +1,15 @@
-function reddenPage() {
-   
-    var a = 'Bolsonaro';
-    var b = 'Idiota';
-
-
-    if(window.find)
-    {
-      while(window.find(a))
-      {
-        var rng=window.getSelection().getRangeAt(0);
-        rng.deleteContents();
-        rng.insertNode(document.createTextNode(b));
-      }
-    }
-    else if(document.body.createTextRange)
-    {
-      var rng=document.body.createTextRange();
-      while(rng.findText(a))
-      {
-        rng.pasteHTML(b);
-      }
-    }
-    window.scrollTo(0,0);
-  }
-  
-  chrome.action.onClicked.addListener((tab) => {
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: reddenPage
-    });
-  });
-
-
-
-  
+const names = ['Bolsonaro'],
+      elements = [...document.getElementsByTagName('*')],
+      nickname = 'Idiota',
+      getRandomPosition = array => Math.floor(Math.random() * array.length) - 0 || 7,
+      getRandomNickname = array => array[getRandomPosition(array)],
+      main = () => elements.forEach((element) => { // code by @pauladiniz - https://github.com/pauladiniz/loremipsum4president <- this girl is awesome <3
+        let validNodes = [...element.childNodes].filter(n => n.nodeType === 3)
+        validNodes.forEach((node) => {
+          let text = node.nodeValue,
+              politicsSUX = text.replace(new RegExp(names.join('|'), 'ig'), 
+              nickname) // here i put my function to get random nicknames :p
+          politicsSUX !== text ? node.nodeValue = politicsSUX : ''
+        })
+      })
+      main()
